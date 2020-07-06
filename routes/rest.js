af5d0d510d645583f431fe99a181ac59df296725
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-07-06 10:54:01 +0300

    Поиск категорий и товаров

--- routes/rest.js
+++ routes/rest.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const Client = require('./../libs/mongoClient');
 
 const router = express.Router();
 
 router
-  .get('/cat/:categoryId', async (req, res) => res.end(`Category: ${req.params.categoryId}`))
+  .get('/cat/:categoryId', async (req, res) => {
+    const category = await Client.findOne(req.params.categoryId, 'category');
+    res.send(category);
+  })
 
-  .get('/p/:productId', async (req, res) => res.end(`Product: ${req.params.productId}`));
+  .get('/p/:productId', async (req, res) => {
+    const product = await Client.findOne(req.params.productId, 'product');
+    res.send(product);
+  });
 
 module.exports = router;
