2a8668697073b9794610c9e27351f2f1cf1112d6
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-08-07 11:24:06 +0300

    Pagination: обработка запрос с пагинацией

--- routes/rest.js
+++ routes/rest.js
@@ -5,6 +5,14 @@ const Client = require('./../libs/mongoClient');
 
 const router = express.Router();
 
+const getProducts = url => new Promise((resolve, reject) => {
+  fetch(url)
+    .then(response => response.json())
+    .then(json => resolve(json))
+    .catch(e => reject(e));
+});
+
+
 router
   .get('/category/:categoryId', async (req, res) => {
     const category = await Client.findOne(req.params.categoryId, 'category');
@@ -17,11 +25,23 @@ router
   })
 
   .get('/products/:categoryId', async (req, res) => {
-    fetch(
-      `https://sik.search.blue.cdtapps.com/ru/ru/product-list-page?category=${req.params.categoryId}&sort=RELEVANCE&size=24&subcategories-style=tree-navigation&c=plp`
-    )
-      .then(response => response.json())
-      .then(json => res.send(json.productListPage));
+    const PER_PAGE = 24;
+    const { categoryId } = req.params;
+    const page = parseInt(req.query.page, 10);
+
+    if (page && page !== 1) {
+      const end = page * PER_PAGE;
+      const start = end - PER_PAGE;
+
+      const resultProducts = await getProducts(`https://sik.search.blue.cdtapps.com/ru/ru/product-list-page?category=${categoryId}&sort=RELEVANCE&size=24&subcategories-style=tree-navigation&c=plp`);
+      const resultMoreProducts = await getProducts(`http://sik.search.blue.cdtapps.com/ru/ru/product-list-page/more-products?&category=${categoryId}&sort=RELEVANCE&start=${start}&end=${end}&c=plp`);
+
+      res.send(Object.assign({}, resultProducts.productListPage, resultMoreProducts.moreProducts));
+    }
+    else {
+      const result = await getProducts(`https://sik.search.blue.cdtapps.com/ru/ru/product-list-page?category=${categoryId}&sort=RELEVANCE&size=24&subcategories-style=tree-navigation&c=plp`)
+      res.send(result.productListPage);
+    }
   })
 
   .get('/search/products', async (req, res) => {
