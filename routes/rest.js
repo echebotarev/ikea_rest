6e30e44b4aa908be8a1b5b8bd002fccd7be46064
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-08-16 15:55:25 +0300

    Rest: наличие товара

--- routes/rest.js
+++ routes/rest.js
@@ -85,9 +85,31 @@ router
   })
 
   .get('/available', async (req, res) => {
-    const url = `${config.get('availableUrl')}?url=${req.query.url}`;
-    fetch(url)
-      .then(response => response.json())
+    /**
+     * Возможно время от времение надо менять Contract и Consumer'a
+     * Эти данные можно посмотреть в
+     * window.Checkout.settings.shoppinglist.contract
+     * window.Checkout.settings.shoppinglist.consumer
+     * */
+
+    const { type } = req.query;
+    const id = req.query.id.replace('s', '');
+    const url = `https://iows.ikea.com/retail/iows/ru/ru/stores/524/availability/${type}/${id}`;
+    fetch(url, {
+      headers: {
+        Authority: 'iows.ikea.com',
+        Accept: 'application/vnd.ikea.iows+json;version=1.0',
+        Origin: 'https://order.ikea.com',
+        Consumer: 'MAMMUT',
+        Contract: '37249'
+      }
+    })
+      .then(response => {
+        console.log('Response', response);
+        console.log('Headers', response.headers);
+
+        return response.json();
+      })
       .then(json => res.send(json));
   });
 
