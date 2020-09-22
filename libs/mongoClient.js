698c6db18d9c4410a3efd07d06ca90cc2b25b9c4
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-09-22 11:02:47 +0300

    Автоматическое вычисление имени базы

--- libs/mongoClient.js
+++ libs/mongoClient.js
@@ -1,5 +1,7 @@
 const { MongoClient } = require('mongodb');
 
+const dayjs = require('dayjs');
+
 const assert = require('assert');
 const config = require('./config');
 
@@ -35,15 +37,30 @@ const Client = {
   }
 };
 
+function getDbName(shift = 0, dbs) {
+  const date = dayjs().subtract(shift * 24, 'hour').format('DDMMYYYY');
+  const db = dbs.find(dbItem => dbItem.name.includes(date));
+
+  if (db) {
+    return db.name;
+  }
+
+  return getDbName(shift + 1, dbs);
+}
+
 // Use connect method to connect to the server
 MongoClient.connect(
   config.get('mongo:uri'),
   { useUnifiedTopology: true },
-  (err, client) => {
+  async (err, client) => {
     assert.equal(null, err);
     console.log('Connected successfully to server');
 
-    Client.db = client.db(config.get('mongo:dbname'));
+    const listDbs = await client.db('Test').admin().listDatabases();
+    const dbname = getDbName(0, listDbs.databases);
+
+    Client.db = client.db(dbname);
+
     Client.db
       .collection(config.get('mongo:productCollectionName'))
       .createIndex(
