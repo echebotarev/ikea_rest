fe6f2d0f81eb82b8f3ef571e789542bea501fcbd
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-12-20 08:52:34 +0300

    TimeToDelivery: на новогодние праздники

--- handlers/timeToDelivery.js
+++ handlers/timeToDelivery.js
@@ -5,15 +5,18 @@ dayjs.locale('ru');
 
 
 function getDeliveryDay(startDay) {
-  const START_DAY = '2020-11-15T00:00:00+05';
+  const START_DAY = '2020-12-21T00:00:00+05';
   const deliveryDay = startDay ? startDay.add(7, 'day') : dayjs(START_DAY);
   const nowDay = dayjs();
 
-  if (deliveryDay.diff(nowDay, 'day') > 7) {
-    return deliveryDay.format('DD MMMM');
-  }
-
-  return getDeliveryDay(deliveryDay);
+  // на период новогодних праздников
+  return '14 января';
+  //
+  // if (deliveryDay.diff(nowDay, 'day') > 7) {
+  //   return deliveryDay.format('DD MMMM');
+  // }
+  //
+  // return getDeliveryDay(deliveryDay);
 }
 
 module.exports = getDeliveryDay;
