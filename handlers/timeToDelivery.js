7e0f89e676f9ff06ff3c366cd249defb0b204c0a
Egor Chebotarev <eg.chebotarev@gmail.com>
2020-12-26 17:24:40 +0300

    Дата доставки на НГ

--- handlers/timeToDelivery.js
+++ handlers/timeToDelivery.js
@@ -3,14 +3,13 @@ require('dayjs/locale/ru');
 
 dayjs.locale('ru');
 
-
 function getDeliveryDay(startDay) {
   const START_DAY = '2020-12-21T00:00:00+05';
   const deliveryDay = startDay ? startDay.add(7, 'day') : dayjs(START_DAY);
   const nowDay = dayjs();
 
   // на период новогодних праздников
-  return '30 декабря';
+  return '17 января';
   //
   // if (deliveryDay.diff(nowDay, 'day') > 7) {
   //   return deliveryDay.format('DD MMMM');
