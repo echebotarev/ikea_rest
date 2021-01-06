43accb8b025acdd9fa767180f5a20c336cc2f75e
Egor Chebotarev <eg.chebotarev@gmail.com>
2021-01-06 08:47:24 +0300

    Ориентировочная дата доставки

--- handlers/timeToDelivery.js
+++ handlers/timeToDelivery.js
@@ -4,18 +4,15 @@ require('dayjs/locale/ru');
 dayjs.locale('ru');
 
 function getDeliveryDay(startDay) {
-  const START_DAY = '2020-12-21T00:00:00+05';
+  const START_DAY = '2020-12-20T00:00:00+05';
   const deliveryDay = startDay ? startDay.add(7, 'day') : dayjs(START_DAY);
   const nowDay = dayjs();
 
-  // на период новогодних праздников
-  return '17 января';
-  //
-  // if (deliveryDay.diff(nowDay, 'day') > 7) {
-  //   return deliveryDay.format('DD MMMM');
-  // }
-  //
-  // return getDeliveryDay(deliveryDay);
+  if (deliveryDay.diff(nowDay, 'day') > 7) {
+    return deliveryDay.format('DD MMMM');
+  }
+
+  return getDeliveryDay(deliveryDay);
 }
 
 module.exports = getDeliveryDay;
