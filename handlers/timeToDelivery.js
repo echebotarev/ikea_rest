7ee7004166ca5ad1440055f242f08e6584145cab
Egor Chebotarev <eg.chebotarev@gmail.com>
2021-01-26 11:36:35 +0300

    Отодвинул даты доставки

--- handlers/timeToDelivery.js
+++ handlers/timeToDelivery.js
@@ -4,15 +4,15 @@ require('dayjs/locale/ru');
 dayjs.locale('ru');
 
 function getDeliveryDay(startDay) {
-  // в 14:00 воскресенья начнет показываться время следующей недели.
-  const START_DAY = '2021-01-24T14:00:00+05';
+  // в 00:00 субботы начнет показываться время следующей недели.
+  const START_DAY = '2021-01-24T17:00:00+05';
   const deliveryDay = startDay ? startDay.add(7, 'day') : dayjs(START_DAY);
   const nowDay = dayjs();
 
-  if (deliveryDay.diff(nowDay, 'day') >= 7) {
+  if (deliveryDay.diff(nowDay, 'day') > 7) {
     return {
       deliveryDay: deliveryDay.format('DD MMMM'),
-      lastOrderDay: deliveryDay.subtract(7, 'day').format('DD MMMM')
+      lastOrderDay: deliveryDay.subtract(8, 'day').format('DD MMMM')
     };
   }
 
