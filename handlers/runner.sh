06ac9f2e63476e6e27076a6009d5fea63b4a8e01
Egor Chebotarev <eg.chebotarev@gmail.com>
2021-03-06 13:53:10 +0300

    Запуск node через cron

--- handlers/runner.sh
+++ handlers/runner.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env sh
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use 12.18.3
 node /var/www/ikea_rest/handlers/createFbFeed.js > /root/fbFeed.log 2>&1
 node /var/www/ikea_rest/handlers/createYmlCatalog.js > /root/yml.log 2>&1
 node /var/www/ikea_rest/handlers/createSitemap.js > /root/sitemap.log 2>&1
