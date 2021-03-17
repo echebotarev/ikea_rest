fea2137941d30e924d9003bb1cd40909bc0a2ca5
Egor Chebotarev <eg.chebotarev@gmail.com>
2021-03-17 09:42:05 +0300

    Available: расширение getAvailable до возможности принять массив

--- libs/getAvailable.js
+++ libs/getAvailable.js
@@ -3,12 +3,25 @@ const config = require('../libs/config');
 
 const handleError = require('./handleError');
 
-module.exports = async ({ type, id }) => {
-  const url = `${config.get('availableUrl')}/product?type=${type}&id=${id}`;
+module.exports = async payload => {
+  let url = null;
+  let products = null;
+
+  if (Array.isArray(payload)) {
+    url = `${config.get('availableUrl')}/products`;
+    products = payload;
+  } else {
+    const { type, id } = payload;
+    url = `${config.get('availableUrl')}/product?type=${type}&id=${id}`;
+  }
+
   // eslint-disable-next-line no-return-await
-  return await fetch(url)
+  return await fetch(url, {
+    method: products ? 'POST' : 'GET',
+    body: products
+  })
     .then(response => response.json())
-    .catch((err) => {
+    .catch(err => {
       handleError(err);
       return {};
     });
