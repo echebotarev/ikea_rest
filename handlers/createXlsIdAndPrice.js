9f70c272d5f5bdeb2bc8c341ce1b64453e2dbde6
Egor Chebotarev <eg.chebotarev@gmail.com>
2021-07-02 18:57:55 +0300

    Excel: создание простого примера

---
+++ handlers/createXlsIdAndPrice.js
@@ -0,0 +1,25 @@
+const excel = require('excel4node');
+
+const Client = require('./../libs/mongoClient');
+const getPrice = require('./../handlers/price');
+
+const workbook = new excel.Workbook();
+const worksheet = workbook.addWorksheet('Sheet 1');
+
+const createXls = async () => {
+  const products = await Client.get('product');
+  products.forEach((product, index) => {
+    worksheet.cell(index + 1, 1).string(product.identifier);
+    worksheet
+      .cell(index + 1, 2)
+      .number(getPrice(product.price.price.mainPriceProps.price.integer));
+  });
+
+  workbook.write('Excel.xlsx');
+
+  console.log('Done');
+};
+
+setTimeout(async () => {
+  await createXls();
+}, 2000);
