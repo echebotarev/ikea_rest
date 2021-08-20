41ae4af5c1a0b570160ee39929d739a7df1e6eed
Egor Chebotarev <eg.chebotarev@gmail.com>
2021-08-20 08:59:05 +0300

    Available: правильная проверка наличия

--- routes/available.js
+++ routes/available.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const { samaraShopId } = require('./../constant');
 
+const getShopData = require('./../utils/getShopData');
+
 const getAvailable = require('./../libs/getAvailable');
 const updateProducts = require('../libs/updateProducts');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const { ikeaShopId = samaraShopId } = req.cookies;
+  const { ikeaShopId = samaraShopId } = getShopData(req);
   const { type, id } = req.query;
 
   const result = await getAvailable({ type, id, ikeaShopId });
