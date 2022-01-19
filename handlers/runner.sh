ef7d402b27ad403369a37d3fdd4030a74f8093e5
Egor Chebotarev <eg.chebotarev@gmail.com>
2022-01-19 09:09:51 +0300

    Sitemap: Runner

--- handlers/runner.sh
+++ handlers/runner.sh
@@ -4,5 +4,6 @@ nvm use 12.18.3
 node /var/www/ikea_rest/handlers/createFbFeed.js > /root/fbFeed.log 2>&1
 node /var/www/ikea_rest/handlers/createYmlCatalog.js > /root/yml.log 2>&1
 node /var/www/ikea_rest/handlers/createSitemap.js > /root/sitemap.log 2>&1
+node /var/www/ikea_rest/handlers/createSitemap.js 001 002 003 004 > /root/sitemap-domains.log 2>&1
 node /var/www/ikea_rest/handlers/createKaspiKzFeed.js > /root/kaspiFeed.log 2>&1
 node /var/www/ikea_rest/handlers/createKaspiKzFeed.js 004 > /root/kaspiFeed.log 2>&1
