c4dc9eeade4d2beef24606ba787242cd02e4d3e6
Egor Chebotarev <eg.chebotarev@gmail.com>
2022-02-26 13:39:54 +0300

    DB: баг в запуске скрипта

--- handlers/run-update-prices.sh
+++ handlers/run-update-prices.sh
@@ -2,4 +2,4 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm use 12.18.3
 
-node /var/www/ikea_rest/handlers/updateProductDbPrices.js.js > /root/updateProductDbPrices.log 2>&1
+node /var/www/ikea_rest/handlers/updateProductDbPrices.js > /root/updateProductDbPrices.log 2>&1
